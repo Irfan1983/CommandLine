0e43254c8efaed737f043572f371a9b77390cfcb
Irfan1983 <34341287+Irfan1983@users.noreply.github.com>
2022-02-22 21:36:16 +0530

    test

---
+++ Project_Google/src/com/stepdefinition/GoogleSearch.java
@@ -0,0 +1,43 @@
+package com.stepdefinition;
+
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import com.utility.PropertiesReader;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class GoogleSearch {
+	
+	
+	
+	PropertiesReader obj=new PropertiesReader();
+	
+	@Given("^open browser$")
+	public void open_chrome_browser_and_enter_url() throws Throwable 
+	{
+		System.out.println("open browser");
+	    System.out.println();
+	    //WebDriverManager.edgedriver().setup();   
+		// EdgeDriver driver = new EdgeDriver();
+
+		 //  Properties properties=obj.getProperty();
+		  // driver.get(properties.getProperty("BaseURL"));	
+		  //System.out.println(properties.getProperty("BaseURL"));
+		  // Thread.sleep(5000);
+	    System.out.println("Executed from command line");
+	}
+	
+	
+	@Then("^Enter criteria$")
+	public void Entercriteria() 
+	{
+		//driver.findElement(By.xpath(null))
+	}
+}
